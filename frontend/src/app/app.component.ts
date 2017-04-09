985b5814526a15ea526b6d0298e83c13757f9357
jenkrisu <jenni.unkuri@gmail.com>
2017-04-10 02:17:39 +0300

    Remove test onClick method from app.component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,29 +1,10 @@
 import { Component } from '@angular/core';
-import { Http } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-
-  constructor(private http: Http) {}
-
-  onClick() {
-this.http.get('/api/products').map(res => res.json()).subscribe(
-      data => {
-        console.log('data')
-        console.log(data._embedded.products)
-      },
-      error => {
-        console.log('error')
-        console.log(error)
-      }
-
-    );
-  }
-
 }
