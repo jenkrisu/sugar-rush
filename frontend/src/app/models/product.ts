a32f43ea2d6d8a20762ee4d28888a7dd6d1d8e34
Ville Haapavaara <ville.haapavaara@cs.tamk.fi>
2017-04-09 13:43:23 +0300

    Add product component

---
+++ frontend/src/app/models/product.ts
@@ -0,0 +1,20 @@
+class Product {
+  id:number;
+  title:string;
+  desc:string;
+  ingredients:string;
+  weight:number;
+  price:number;
+  stock:number;
+  isNew:boolean;
+
+  constructor(id:number, title:string, desc:string, ingredients:string, weight:number, price:number, stock:number, isNew:boolean) {
+    this.id = id;
+    this.title = title;
+    this.ingredients = ingredients;
+    this.weight = weight;
+    this.price = price;
+    this.stock = stock;
+    this.isNew = isNew;
+  }
+}
