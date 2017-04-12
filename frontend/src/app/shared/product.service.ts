1f7b1934c94018ee1ecb57dcfb981b4caf9094a3
jenkrisu <jenni.unkuri@gmail.com>
2017-04-12 14:31:36 +0300

    Add shopping car service

--- frontend/src/app/shared/product.service.ts
+++ frontend/src/app/shared/product.service.ts
@@ -1,32 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import 'rxjs/add/operator/map';
-
-import { ShoppingCart } from '../models/shopping-cart';
-import { CartItem } from '../models/cart-item';
-import { Product } from '../models/product';
 
 @Injectable()
 export class ProductService {
 
-  private sbtShoppingCart = 
-    new BehaviorSubject<ShoppingCart>(new ShoppingCart(0, []));
-
   constructor(private http: Http) {
-    if (sessionStorage.getItem('sbtShoppingCart') != null) {
-      const cart = JSON.parse(sessionStorage.getItem('sbtShoppingCart'));
-      this.sbtShoppingCart.next(cart);
-    }
-  }
-
-  addToShoppingCart(product: Product) {
-    return false;
-  }
-
-  getShoppingCart() {
-    return this.sbtShoppingCart;
   }
 
   getProducts() {
