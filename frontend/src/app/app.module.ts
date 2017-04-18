4e2cbb4824f91269f93237ed015254e5f2014e16
jenkrisu <jenni.unkuri@gmail.com>
2017-04-18 13:27:13 +0300

    Refactor checkout component to shoppingcart component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { ProductComponent } from './product/product.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { NewProductsComponent } from './new-products/new-products.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { CheckoutComponent } from './checkout/checkout.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 import { ProductService } from './shared/product.service';
 import { ShoppingCartService } from './shared/shopping-cart.service';
@@ -22,7 +22,7 @@ import { ShoppingCartService } from './shared/shopping-cart.service';
     ProductListComponent,
     NewProductsComponent,
     ProductDetailComponent,
-    CheckoutComponent
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
