c590cf5f143b35016ced43129accb4136b3dfdc4
jenkrisu <jenni.unkuri@gmail.com>
2017-04-23 16:03:20 +0300

    Add checkout component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { ProductListComponent } from './product-list/product-list.component';
 
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 const routes: Routes = [
     { path: '', component: NewProductsComponent },
     { path: 'products', component: ProductListComponent },
     { path: 'products/:id', component: ProductDetailComponent },
     { path: 'shoppingcart', component: ShoppingCartComponent },
+    { path: 'checkout', component: CheckoutComponent },
     { path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
