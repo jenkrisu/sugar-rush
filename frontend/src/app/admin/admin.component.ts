6deff4b50bf119d8e56252f9b2455e4642f61a2d
Ville Haapavaara <ville.haapavaara@cs.tamk.fi>
2017-04-24 20:47:15 +0300

    Add a function to update a product

--- frontend/src/app/admin/admin.component.ts
+++ frontend/src/app/admin/admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../shared/product.service';
+import { Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
@@ -10,11 +11,43 @@ export class AdminComponent implements OnInit {
 
   showAddProduct: boolean = false;
   products;
+  tmpProduct = {
+    "id": null,
+    "title": null,
+    "description": null,
+    "ingredients": null,
+    "weight": null,
+    "price": null,
+    "stock": null,
+    "image": null,
+    "categories": null,
+    "created": null
+  };
+  form;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.getProducts();
+    this.form = this.formBuilder.group({
+      id: this.formBuilder.control(''),
+      title: this.formBuilder.control('' /*, Validators.compose([
+       Validators.required,
+       Validators.pattern('[\\w\\-\\s\\/]+')
+       ]) */),
+      description: this.formBuilder.control(''),
+      ingredients: this.formBuilder.control(''),
+      weight: this.formBuilder.control(''),
+      price: this.formBuilder.control(''),
+      stock: this.formBuilder.control('' /*, Validators.compose([
+       Validators.required,
+       Validators.pattern('^[0-9]*$')
+       ])*/),
+      image: this.formBuilder.control(''),
+      categories: this.formBuilder.control(''),
+      created: this.formBuilder.control('')
+    });
   }
 
   changeAddProductStatus() {
@@ -37,16 +70,49 @@ export class AdminComponent implements OnInit {
       );
   }
 
-  editProduct(product) {
-    console.log("Edit: " + JSON.stringify(product));
+  resetTmpProduct() {
+    this.tmpProduct = {
+      "id": null,
+      "title": null,
+      "description": null,
+      "ingredients": null,
+      "weight": null,
+      "price": null,
+      "stock": null,
+      "image": null,
+      "categories": null,
+      "created": null
+    };
+  }
 
+  expandProduct(product) {
+    this.tmpProduct = product;
+    this.form.controls['id'].setValue(this.tmpProduct.id);
+    this.form.controls['title'].setValue(this.tmpProduct.title);
+    this.form.controls['description'].setValue(this.tmpProduct.description);
+    this.form.controls['ingredients'].setValue(this.tmpProduct.ingredients);
+    this.form.controls['weight'].setValue(this.tmpProduct.weight);
+    this.form.controls['price'].setValue(this.tmpProduct.price);
+    this.form.controls['stock'].setValue(this.tmpProduct.stock);
+    this.form.controls['image'].setValue(this.tmpProduct.image);
+    this.form.controls['categories'].setValue(this.tmpProduct.categories);
+    this.form.controls['created'].setValue(this.tmpProduct.created);
+  }
+
+  editProduct(product) {
+    console.log("onSubmit");
+    console.log("Product: " + JSON.stringify(product));
+    this.productService.updateProduct(product)
+      .subscribe((data) => {
+        window.location.reload();
+      });
   }
 
   deleteProduct(product) {
     console.log("Delete: " + product);
     this.productService.deleteProduct(product)
       .subscribe((data) => {
-        window.location.reload(true);
+        window.location.reload();
       });
   }
 }
