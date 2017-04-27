ae51ecb244c3855cd918b5ca7e9545859a823377
jenkrisu <jenni.unkuri@gmail.com>
2017-04-28 02:35:46 +0300

    Add post methods for purchase

--- frontend/src/app/shared/product.service.ts
+++ frontend/src/app/shared/product.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 import { Http, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+
+import { Cart } from '../models/cart';
+import { Customer } from '../models/customer';
+import { Address } from '../models/address';
 import { Product } from '../models/product';
 
 @Injectable()
@@ -37,4 +41,47 @@ export class ProductService {
     let uri:string = "api/products/" + product.id;
     return this.http.delete(uri);
   }
+
+  // Purchasing products as new customer
+  purchaseProducts(customer: Customer, cart: Cart) {
+     let stringifiedCart = this.stringifyCart(cart);
+     
+     // TODO: Delivery address would be different if user is selected different delivery address
+     let deliveryAddress = new Address(customer.address.street, customer.address.city, customer.address.postal, customer.address.country);
+
+     let purchase = {
+       customer: customer,
+       cart: stringifiedCart,
+       address: deliveryAddress
+     }
+
+     return this.http.post("/api/purchase/new", purchase);
+  }
+
+  // Purchasing products as old custome
+  purchaseProductsLoggedIn(customer: Customer, cart: Cart) {
+    // TODO:
+    // Id as header?
+    let stringifiedCart = this.stringifyCart(cart);
+     
+    // TODO: Delivery address would be different if user is selected different delivery address
+    let deliveryAddress = new Address(customer.address.street, customer.address.city, customer.address.postal, customer.address.country);
+
+    let purchase = {
+      cart: stringifiedCart,
+      address: deliveryAddress
+    }
+
+    return this.http.post("/api/purchase/old", purchase);
+  }
+
+  stringifyCart(cart: Cart) {
+     let stringifiedItems = JSON.stringify(cart.items);
+     let stringifiedCart = {
+       items: stringifiedItems,
+       total: cart.total
+     }
+     return stringifiedCart;
+  }
+
 }
