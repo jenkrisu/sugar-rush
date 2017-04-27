a023a859947d8b885f96137702a85177beecf634
jenkrisu <jenni.unkuri@gmail.com>
2017-04-27 21:37:52 +0300

    Set contact info form fields to remember user input

--- frontend/src/app/shopping-cart/contact-information.component.ts
+++ frontend/src/app/shopping-cart/contact-information.component.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, DoCheck } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+
+import { Address } from '../models/address';
+import { Customer } from '../models/customer';
+
+import { CustomerService } from '../shared/customer.service';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './contact-information.component.html',
   styleUrls: ['./contact-information.component.scss', './shopping-cart.component.scss']
 })
-export class CheckoutComponent implements OnInit {
+export class CheckoutComponent implements OnInit, OnDestroy, DoCheck {
 
-  constructor() { }
+  public firstName: String;
+  public lastName: String;
+  public street: String;
+  public postal: String;
+  public city: String;
+  public country: String;
+  public email: String;
 
+  private customer: Customer;
+  private subscription: Subscription;
+
+  constructor(private customerService: CustomerService) { }
+
+  // Subscribe to Customer BehaviorSubject
   ngOnInit() {
+    this.subscription = this.customerService
+      .getCustomer()
+      .subscribe(item => this.customer = item);
+    this.fillContactForm();
+  }
+
+  // Update the BehaviorSubject when changes to input fields occur
+  ngDoCheck() {
+    if (this.customer !== undefined) {
+      this.updateCustomer();
+    }
+  }
+
+  // Unsubscribe
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  // Create customer and update it to ShoppingCartService
+  updateCustomer() {
+    const address = new Address(this.street, this.postal, this.city, this.country);
+    let c = new Customer(address, this.firstName, this.lastName, this.email);
+    this.customerService.setCustomer(c);
+  }
+
+  // Fill contact information form with previously inputted data
+  fillContactForm() {
+    this.street = this.customer.address.street;
+    this.postal = this.customer.address.postal;
+    this.city = this.customer.address.city;
+    this.country = this.customer.address.country;
+    this.firstName = this.customer.firstName;
+    this.lastName = this.customer.lastName;
+    this.email = this.customer.email;
   }
 
 }
