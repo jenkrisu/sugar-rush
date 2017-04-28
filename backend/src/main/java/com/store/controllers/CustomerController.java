c2c00d1f93638090842bff88856fddba26f9c02d
Ville Haapavaara <ville.haapavaara@cs.tamk.fi>
2017-04-28 11:12:09 +0300

    Add controller specific annotations

--- backend/src/main/java/com/store/controllers/CustomerController.java
+++ backend/src/main/java/com/store/controllers/CustomerController.java
@@ -1,11 +1,16 @@
 package com.store.controllers;
 
 import com.store.repositories.CustomerRepository;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by Ville on 27.4.2017.
  */
+@RepositoryRestController
+@RequestMapping("/customers")
 public class CustomerController {
 
     private CustomerRepository customerRepository;
+
 }
