e695f5adbe27046d9568ddad0fab3741c149003d
Ville Haapavaara <ville.haapavaara@cs.tamk.fi>
2017-04-28 11:05:38 +0300

    Add repository annotation

--- backend/src/main/java/com/store/repositories/AccountRepository.java
+++ backend/src/main/java/com/store/repositories/AccountRepository.java
@@ -2,9 +2,11 @@ package com.store.repositories;
 
 import com.store.entities.Account;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * Created by Ville on 27.4.2017.
  */
+@RepositoryRestResource
 public interface AccountRepository extends CrudRepository<Account, Integer> {
 }
--- backend/src/main/java/com/store/repositories/CustomerRepository.java
+++ backend/src/main/java/com/store/repositories/CustomerRepository.java
@@ -2,9 +2,11 @@ package com.store.repositories;
 
 import com.store.entities.Customer;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * Created by Jenni on 24.4.2017.
  */
+@RepositoryRestResource
 public interface CustomerRepository extends CrudRepository<Customer, Integer>{
 }
