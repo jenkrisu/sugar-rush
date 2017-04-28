f086aa75f142419aeea8e9508af99171859a4898
jenkrisu <jenni.unkuri@gmail.com>
2017-04-28 18:12:00 +0300

    Add title

--- frontend/src/app/models/simple-cart-item.ts
+++ frontend/src/app/models/simple-cart-item.ts
@@ -1,10 +1,12 @@
 export class SimpleCartItem {
 
   id: number;
+  title: string;
   amount: number;
 
-  constructor(id: number, amount: number) {
+  constructor(id: number, title: string, amount: number) {
       this.id = id;
+      this.title = title;
       this.amount = amount;
   }
 
